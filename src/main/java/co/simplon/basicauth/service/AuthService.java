5fda81616140fa711db5b20ef1e4ec071ba2ba2d
DEHAINAULT Quentin <quentin.dehainault@maif.fr>
2026-08-24 13:46:04 +0200

    feat: Ajoute de l'authentification basique

---
+++ src/main/java/co/simplon/basicauth/service/AuthService.java
@@ -0,0 +1,25 @@
+package co.simplon.basicauth.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import co.simplon.basicauth.repository.UserRepository;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepositoryInjected) {
+        this.userRepository = userRepositoryInjected;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return this.userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username " + username));
+    }
+}
